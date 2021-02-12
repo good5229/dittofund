01244ee5326b419cff042275a834f6f70ab9bea0
bellhundred <good5229@naver.com>
2021-02-12 17:43:02 +0900

    feat(listview): add listview on Hedgefund and Portfolio

--- index/urls.py
+++ index/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from index.views import Index, HedgefundView, CompareView, PortfolioView
+from index.views import Index, CompareView, PortfolioList, HedgefundList
 
 app_name = 'index'
 
 urlpatterns = [
     path('', Index.as_view(), name='intro'),
-    path('hedgefund/', HedgefundView.as_view(), name='hedgefund'),
+    path('hedgefund/', HedgefundList.as_view(), name='hedgefund'),
     path('compare/', CompareView.as_view(), name='compare'),
-    path('portfolio/', PortfolioView.as_view(), name='portfolio'),
+    path('portfolio/', PortfolioList.as_view(), name='portfolio'),
 ]
